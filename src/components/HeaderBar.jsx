265142ad6e4419cc2b9ce0c9f1d08b178ac4962c
Tommaso Amato <tommaso.amato1993@gmail.com>
2023-07-26 19:46:10 +0200

    mod layout tricolore

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -14,7 +14,7 @@ export const HeaderBar = () => {
       {/* <Header small={false} theme="" type="slim">
 
       </Header> */}
-    <div className="row m-0 bg-tertiary" >
+      <div className="row m-0 bg-tertiary" >
         {/* <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start logo-section pt-2 pb-2">
             <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
             <p className="pt-2">
@@ -32,31 +32,32 @@ export const HeaderBar = () => {
             </p>
         </div> */}
         <div className="col-12  d-flex justify-content-md-center justify-content-sm-start logo-section pt-2 pb-2 pr-sm-3">
-            <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
-            <p className="pt-2">
-                <a href="http://www.salute.gov.it/portale/home.html" className="text-decoration-none" aria-label="Ministero della Salute" rel="noreferrer" alt="Ministero della Salute">
-                Ministero della Salute
-                </a>
-            </p>
+          <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          <p className="pt-2">
+            <a href="http://www.salute.gov.it/portale/home.html" className="text-decoration-none" aria-label="Ministero della Salute" rel="noreferrer" alt="Ministero della Salute">
+              Ministero della Salute
+            </a>
+          </p>
         </div>
+         {/** bandiera italiana*/}
+        <div className="container-fluid">
+            <div className="row" >
+              <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start" style={{ backgroundColor: '#009246', height: '8px' }}>&nbsp;</div>
+              <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start" style={{ backgroundColor: '#ffffff', height: '8px' }}>&nbsp;</div>
+              <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start" style={{ backgroundColor: '#ce2b37', height: '8px' }}>&nbsp;</div>
+            </div>
+          </div>
         {/** TITLE + LOGO*/}
         <div className="col-12 d-flex flex-column justify-content-center mainBanner">
-        <div className="d-flex justify-content-center align-items-center"> {/* Aggiunto align-items-center per allineare verticalmente gli elementi */}
-        {/* <img src="Coccarda.svg" alt="Logo" height="150px" className="main-logo" /> */}
-        <h1 className="large-title pl-2 pt-4 d-lg-none d-md-none d-xs-block">Report Vaccini Anti COVID-19</h1> {/* Title Mobile*/}
-        <h1 className="page-title large-title pl-2 pt-5 d-none d-md-block">Report Vaccini Anti COVID-19</h1>  {/* Title Desktop*/}
-        <a href="https://www.salute.gov.it/portale/nuovocoronavirus/homeNuovoCoronavirus.jsp" target="_blank" style={{backgroundColor: '#ffffff', color: '#0066CC', padding: '5px 20px', textDecoration: 'none', display: 'inline-block', marginLeft: '20px', borderRadius: '5px', textAlign: 'center', alignSelf: 'center', marginTop: '40px'}}>
-            vai all'area <strong>Covid-19</strong>
-            <FontAwesomeIcon icon={faChevronRight} style={{marginLeft: '10px'}} />
-        </a>
-    </div>
-          <div className="container-fluid">
-            <div className="row" >
-              <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start" style={{backgroundColor:'#009246', height: '8px'}}>&nbsp;</div>
-              <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start" style={{backgroundColor:'#ffffff',height: '8px'}}>&nbsp;</div>
-              <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start" style={{backgroundColor:'#ce2b37',height: '8px'}}>&nbsp;</div>
+          <div className="d-flex justify-content-center align-items-center"> {/* Aggiunto align-items-center per allineare verticalmente gli elementi */}
+            {/* <img src="Coccarda.svg" alt="Logo" height="150px" className="main-logo" /> */}
+            <h1 className="large-title pl-2 pt-4 d-lg-none d-md-none d-xs-block">Report Vaccini Anti COVID-19</h1> {/* Title Mobile*/}
+            <h1 className="page-title large-title pl-2 pt-5 d-none d-md-block">Report Vaccini Anti COVID-19</h1>  {/* Title Desktop*/}
+            <a href="https://www.salute.gov.it/portale/nuovocoronavirus/homeNuovoCoronavirus.jsp" target="_blank" style={{ backgroundColor: '#ffffff', color: '#0066CC', padding: '5px 20px', textDecoration: 'none', display: 'inline-block', marginLeft: '20px', borderRadius: '5px', textAlign: 'center', alignSelf: 'center', marginTop: '40px' }}>
+              vai all'area <strong>Covid-19</strong>
+              <FontAwesomeIcon icon={faChevronRight} style={{ marginLeft: '10px' }} />
+            </a>
           </div>
-        </div>
         </div>
       </div>
     </>
